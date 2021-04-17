2a7c989293dbd3b38edd8f11dda6dbbe6f4ed98d
abhishekg1981992 <33199201+abhishekg1981992@users.noreply.github.com>
2021-04-17 17:58:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://manthanguru-node-app.herokuapp.com",
+  origin: "http://manthanguru.com",
 };
 
 app.use(cors(corsOptions));
